d91d002816d5f66809374d136322d66440ffe2b6
Destinn <donats2004@gmail.com>
2022-12-22 20:24:18 +0300

    address finale

--- Weak4/address.c
+++ Weak4/address.c
@@ -2,8 +2,13 @@
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n; 
+    char *s = "HI!";
+    printf("%s\n", s);
+    char *p = &s[0];
     printf("%p\n", p);
-    printf("%i\n", *p);
+
+    int numbers[] = {1, 2, 5, 7, 4, 8, 5, 9};
+
+    printf("%i\n", numbers[0]);
+    
 }
