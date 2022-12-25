ae639b287f9485fc4d424d226684bd6c7d1a6bde
IDestinn <donats2004@gmail.com>
2022-12-25 12:18:10 +0300

    lab1 in process

---
+++ Weak4/lab1/lab1.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Execute like: ./lab1 input.wav output.wav factor\n");
+        return 1;
+    }
+    
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Input file can't be opened\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Output file can't be opened\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+    
+
+    uint8_t header[HEADER_SIZE];
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+
+    int16_t *buffer;
+    while (0) 
+    {   
+        fread(&buffer, sizeof(int16_t), 1, input);
+
+        if (buffer == NULL)
+        {
+            break;
+        }
+
+        *buffer *= factor;
+        fwrite(&buffer, sizeof(uint8_t), 1, output);
+    }
+
+    fclose(input);
+    fclose(output);
+
+    printf("Succses!");
+    return 0;
+}
